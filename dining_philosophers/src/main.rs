c39bf924947de19fe352ed5731d043bc5b916fe0
unixzen <edemin2@gmail.com>
2019-07-17 18:22:42 +0300

    Fix dining philosophers

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,29 +1,67 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::{Mutex, Arc};
+
 struct Philosopher {
-name: String,
+    name: String,
+    left: usize,
+    right: usize,
+}
+
+struct Table {
+    forks: Vec<Mutex<()>>,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }  
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} начала есть.", self.name);
+        thread::sleep(Duration::from_millis(1000));
         println!("{} закончила есть.", self.name);
     }
 }
 
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
     let philosophers = vec![
-        Philosopher::new("Джудит Батлер"),
-        Philosopher::new("Рая Дунаевская"),
-        Philosopher::new("Зарубина Наталья"),
-        Philosopher::new("Эмма Гольдман"),
-        Philosopher::new("Анна Шмидт"),
+        Philosopher::new("Джудит Батлер", 0, 1),
+        Philosopher::new("Рая Дунаевская", 1, 2),
+        Philosopher::new("Зарубина Наталья", 2, 3),
+        Philosopher::new("Эмма Гольдман", 3, 4),
+        Philosopher::new("Анна Шмидт", 0, 4),
     ];
 
-    for p in &philosophers {
-        p.eat();
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
     }
+
+//    for p in &philosophers {
+//        p.eat();
+//    }
 }
