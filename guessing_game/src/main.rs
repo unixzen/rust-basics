48ba73f9c294032bea9232f9413d70a1229ea83c
unixzen <edemin2@gmail.com>
2019-07-23 09:54:01 +0300

    Fix guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,23 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Число должно быть между 1 и 100, получено {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
 
 fn main() {
     println!("Угадайте число!");
@@ -25,10 +42,15 @@ fn main() {
             Err(_)  => continue,
         };
 
-        if guess < 1 || guess > 100 {
-            println!("Загаданное число должно быть между 1 и 100");
-            continue;
-        }
+        let guess_item: Guess = Guess::new(guess as u32);
+        let guess: u32 = guess_item.value();
+
+        println!("Вы выиграли! {}", guess);
+
+//        if guess < 1 || guess > 100 {
+//            println!("Загаданное число должно быть между 1 и 100");
+//            continue;
+//        }
 
 //        println!("Ваша попытка: {}", guess);
 
