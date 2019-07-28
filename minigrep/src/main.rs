fb35edc65fa56df1cb31e250f123ee741931b420
Evgeny Demin <edemin2@gmail.com>
2019-07-28 23:29:26 +0300

    Add minigrep example

---
+++ minigrep/src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+//    println!("{:?}", args);
+}
+
